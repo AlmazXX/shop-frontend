37a2931ee8c7757c3280a6ba4e80b5213e0ec5e3
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-17 22:45:56 +0600

    productsSlice: Added fetchProducts thunk

--- src/features/products/productsSlice.ts
+++ src/features/products/productsSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 import { Product } from "../../types";
+import { fetchProducts } from "./productsThunk";
 
 interface ProductsState {
   items: Product[];
@@ -16,6 +17,19 @@ export const productsSlice = createSlice({
   name: "products",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.fetchLoading = true;
+      })
+      .addCase(fetchProducts.fulfilled, (state, { payload: products }) => {
+        state.fetchLoading = false;
+        state.items = products;
+      })
+      .addCase(fetchProducts.rejected, (state) => {
+        state.fetchLoading = false;
+      });
+  },
 });
 
 export const productsReducer = productsSlice.reducer;
