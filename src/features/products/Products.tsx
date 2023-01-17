c12c8cd0b128d973b79eb7960d2d48c30609df37
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-17 23:02:06 +0600

    products: Rendered fetched list of products

--- src/features/products/Products.tsx
+++ src/features/products/Products.tsx
@@ -1,7 +1,19 @@
 import { Button, Grid, Typography } from "@mui/material";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import ProductItem from "./components/ProductItem";
+import { selectProducts } from "./productsSlice";
+import { fetchProducts } from "./productsThunk";
 
 const Products = () => {
+  const dispatch = useAppDispatch();
+  const products = useAppSelector(selectProducts);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
   return (
     <Grid container direction="column" spacing={2}>
       <Grid item container justifyContent="space-between" alignItems="center">
@@ -13,6 +25,16 @@ const Products = () => {
             Add product
           </Button>
         </Grid>
+      </Grid>
+      <Grid item container spacing={2}>
+        {products.map((product) => (
+          <ProductItem
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            id={product.id}
+          />
+        ))}
       </Grid>
     </Grid>
   );
