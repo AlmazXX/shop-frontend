aec706ea259273ddae790229c53f57e755a4d3d9
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-17 23:59:10 +0600

    NewProduct: Created onFormSubmit for ProductForm

--- src/features/products/NewProduct.tsx
+++ src/features/products/NewProduct.tsx
@@ -1,14 +1,26 @@
 import { Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../../app/hooks";
+import { ProductMutation } from "../../types";
 import ProductForm from "./components/ProductForm";
+import { createProduct } from "./productsThunk";
 
 const NewProduct = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const onFormSubmit = async (productMutation: ProductMutation) => {
+    await dispatch(createProduct(productMutation));
+    navigate('/')
+  }
+
   return (
     <Grid container direction="column" spacing={2}>
       <Grid item>
         <Typography variant="h4">New product</Typography>
       </Grid>
       <Grid item>
-        <ProductForm />
+        <ProductForm onSubmit={onFormSubmit} />
       </Grid>
     </Grid>
   );
