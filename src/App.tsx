0d95dac496f8b80a185e265858c03f387f6f0914
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-17 23:38:33 +0600

    App: Added route to /products/new

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Container, CssBaseline } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import AppToolbar from "./components/UI/AppToolbar/AppToolbar";
+import NewProduct from "./features/products/NewProduct";
 import Products from "./features/products/Products";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Container maxWidth="xl">
           <Routes>
             <Route path="/" element={<Products />}></Route>
+            <Route path="/products/new" element={<NewProduct />}></Route>
           </Routes>
         </Container>
       </main>
