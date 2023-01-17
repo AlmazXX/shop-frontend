2fccb870e3ee9020e7b2ab0da599665c777069af
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-17 22:28:34 +0600

    App: Added default layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Container, CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import AppToolbar from "./components/UI/AppToolbar/AppToolbar";
+import Products from "./features/products/Products";
+
 function App() {
-  return <div>Hello, world</div>;
+  return (
+    <>
+      <CssBaseline />
+      <header>
+        <AppToolbar />
+      </header>
+      <main>
+        <Container maxWidth="xl">
+          <Routes>
+            <Route path="/" element={<Products />}></Route>
+          </Routes>
+        </Container>
+      </main>
+    </>
+  );
 }
 
 export default App;
