fe554188b2431f139386592af45dc6c3038da9c0
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-01-17 23:58:23 +0600

    productsThunk: Created createProduct

--- src/features/products/productsThunk.ts
+++ src/features/products/productsThunk.ts
@@ -1,8 +1,22 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axiosApi from "../../axiosApi";
-import { Product } from "../../types";
+import { Product, ProductMutation } from "../../types";
 
-export const fetchProducts = createAsyncThunk<Product[]>('products/fetchAll',async () => {
-  const {data} = await axiosApi.get<Product[]>('/products')
-  return data
-})
+export const fetchProducts = createAsyncThunk<Product[]>(
+  "products/fetchAll",
+  async () => {
+    const { data } = await axiosApi.get<Product[]>("/products");
+    return data;
+  }
+);
+
+export const createProduct = createAsyncThunk<void, ProductMutation>(
+  "products/create",
+  async (productMutation) => {
+    const serialized = {
+      ...productMutation,
+      price: parseFloat(productMutation.price),
+    };
+    await axiosApi.post("/products", serialized);
+  }
+);
